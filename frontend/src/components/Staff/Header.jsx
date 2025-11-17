6cbefd3795cb1fdaf30eb1797681db3169ab3b4f
richmondazadze <richmondazadze1313@gmail.com>
2025-11-17 05:13:27 -0600

    staff header fix

--- frontend/src/components/Staff/Header.jsx
+++ frontend/src/components/Staff/Header.jsx
@@ -24,7 +24,7 @@ const StaffHeader = ({ subtitle = 'Staff Workspace' }) => {
 
   return (
     <div className="bg-white rounded-2xl shadow-xl p-6 mb-6 border border-gray-100">
-      <div className="flex flex-col gap-6 lg:flex-row lg:items-center lg:justify-between">
+      <div className="flex flex-wrap items-center justify-between gap-4">
         <div className="flex items-center gap-4">
           <img src={logo} alt="ServeDash logo" className="h-12 w-auto object-contain" />
           <div>
@@ -33,7 +33,7 @@ const StaffHeader = ({ subtitle = 'Staff Workspace' }) => {
           </div>
         </div>
 
-        <div className="flex items-center gap-4 flex-wrap">
+        <div className="flex items-center gap-3 flex-wrap justify-end">
           <div className="relative">
             <Search className="absolute left-3 top-1/2 -translate-y-1/2 text-text-light w-5 h-5" />
             <input
